c89007f7fa52c63d7da577b034b6c4d33f2af9c9
Ruben <ruben@localhost.localdomain>
2018-03-09 18:25:03 -0500

    Post y Delete

--- js/app.js
+++ js/app.js
@@ -1,24 +1,45 @@
 (function ( ) {
 	var app = angular.module('banco',[]);
 
-	app.controller("BancoController",['$http',function($http){
+	app.controller("BancoController",['$http','$scope',function($http,$scope){
 		var banco = this;
 		banco.movimientos = [ ];
 
+	//	$http.get('http://www.titandesarrollo.com/_api/movimientos').success(function(data){
 		$http.get('http://www.titandesarrollo.com/_api/movimientos').success(function(data){
-	//	$http.get('http://localhost/movimiento.json').success(function(data){
 			banco.movimientos = data;
-            console.log(data);
+			console.log(data);
 		});
+
+		$scope.deleteData = function(id){
+			$http({
+				method  : 'DELETE',
+				url     : 'http://www.titandesarrollo.com/_api/movimientos/'+id,
+				//data    : $scope.movimiento,
+				body : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+			})
+		}
 
 	}]);
 
-	app.controller('MovimientoController',['$http' ,function($http){
-		this.movimiento = {};
+	app.controller('MovimientoController',['$http','$scope' ,function($http, $scope){
+		
+		$scope.movimiento = {};
+		$scope.submitForm = function() {
+			console.log($scope.movimiento.tipo_mov);
+			$http({
+				method  : 'POST',
+				url     : 'http://www.titandesarrollo.com/_api/movimientos',
+				data    : $scope.movimiento,
+				body : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+			})
 
-		this.addMovimiento = function(){
-			$http.post('http://www.titandesarrollo.com/_api/movimientos', {param: 'movimiento'} );
-		}
+		};
+
+
+
+
+
 	}]);
 
 })();
